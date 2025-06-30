504c827ef3b2d4a3bfc210df11d78d1b0d01a573
atxbrianbehm <behm.brian@gmail.com>
2025-06-30 16:30:15 -0500

    feat: import generators from feat/engine-enhancements branch

---
+++ generators/index.js
@@ -0,0 +1,16 @@
+// Central generator list for Randomizer
+// Import this file in loader logic to enumerate available generators
+
+export const GENERATOR_FILES = [
+  '/televangelist_generator.json',
+  '/satanic_panic_generator.json',
+  '/anachronisticTechPanel.json',
+  '/deciduous_tree_generator.json' // Added new generator
+];
+
+export const GENERATOR_LABELS = {
+  'Televangelist Generator': 'Televangelist (80s TV)',
+  'Satanic Panic Generator': 'Satanic Panic (80s/90s)',
+  'Anachronistic Tech Panel Generator': 'Tech Panel (Retro-Future)',
+  'Deciduous Tree Prompt Generator': 'Deciduous Tree Prompts' // Added label for new generator
+};
